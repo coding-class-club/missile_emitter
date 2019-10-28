e7d30f32e628dd607a8ec73fe89a80a80cb695e0
Jerry Chen <chen.ruijie@gmail.com>
2019-10-28 17:24:19 +0800

    Initialize the project structure

---
+++ lib/missile_emitter.rb
@@ -0,0 +1,6 @@
+require "missile_emitter/version"
+
+module MissileEmitter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/missile_emitter/version.rb
@@ -0,0 +1,3 @@
+module MissileEmitter
+  VERSION = "0.1.0"
+end

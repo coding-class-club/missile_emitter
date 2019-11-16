3c7c98ea9d5196133a6a9f9851ed27d3c2309b9a
Jerry Chen <chen.ruijie@gmail.com>
2019-11-16 11:07:03 +0800

    forward method missing event to handler

--- lib/missile_emitter/battle_field.rb
+++ lib/missile_emitter/battle_field.rb
@@ -1,7 +1,13 @@
 module MissileEmitter
   class BattleField < BasicObject
+
     def initialize(callable)
       @handler = callable
     end
+
+    def method_missing(msg, *args, &block)
+      @handler.call msg, *args, &block
+    end
+
   end
 end
--- spec/missile_emitter/battle_field_spec.rb
+++ spec/missile_emitter/battle_field_spec.rb
@@ -2,8 +2,7 @@ module MissileEmitter
   RSpec.describe BattleField do
 
     it "继承自 BasicObject 而非 Object" do
-      expect(BattleField).to be < BasicObject
-      expect(BattleField).to_not be < Object
+      expect(BattleField.ancestors).to contain_exactly(BattleField, BasicObject)
     end
 
     it "实例化时接收并保存可调用对象（Callable）" do
@@ -11,6 +10,16 @@ module MissileEmitter
       instance = BattleField.new callable
 
       expect(instance.instance_eval("@handler")).to eq callable
+    end
+
+    it "将 method_missing 事件转交给保存的可调用对象处理" do
+      callable = double "callable"
+
+      expect(callable).to receive(:call).with a_kind_of(Symbol), any_args
+
+      BattleField.new(callable).instance_eval do
+        missing() {}
+      end
     end
 
   end

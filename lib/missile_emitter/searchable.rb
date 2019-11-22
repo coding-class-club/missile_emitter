c63b32cfde34be1e5f97c5bbcbe17b9cf2ebc704
Jerry Chen <chen.ruijie@gmail.com>
2019-11-22 12:07:51 +0800

    增加 Searchable 工具

---
+++ lib/missile_emitter/searchable.rb
@@ -0,0 +1,34 @@
+module MissileEmitter
+  module Searchable
+    
+    # 搜索条件（klass => {field: scope}）
+    # eg. {Person => {name_like: scope, older_than: scope}}
+    conditions = {}
+
+    MissileEmitter do |klass, key, *, &block|
+      (conditions[klass] ||= {}.with_indifferent_access)[key] = block
+    end
+
+    extend ActiveSupport::Concern
+
+    included do
+
+      define_singleton_method :search do |hash|
+        hash.reduce all do |relation, (key, value)|
+          next relation if value.blank? # ignore empty value
+
+          if filter = conditions.fetch(self, {})[key]
+            relation.extending do
+              # Just for fun :)
+              define_method(:_) { value }
+            end.instance_exec(value, &filter)
+          elsif column_names.include?(key)
+            relation.where key => value
+          else
+            relation
+          end
+        end
+      end
+    
+  end
+end

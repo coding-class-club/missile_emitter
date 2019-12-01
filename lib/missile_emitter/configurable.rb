cbf7edaa1bf79072df9a65ed8e78db1d245282a2
Jerry Chen <chen.ruijie@gmail.com>
2019-12-01 14:50:06 +0800

    writer value

--- lib/missile_emitter/configurable.rb
+++ lib/missile_emitter/configurable.rb
@@ -1,11 +1,13 @@
 module MissileEmitter
   module Configurable
     
-    MissileEmitter do |klass, key_field = :key, value_field = :value, key, &block|
-      klass.define_singleton_method key do
+    MissileEmitter do |klass, key_field = :key, value_field = :value, key, &default|
+      klass.define_singleton_method key do |&writer|
         setting = find_or_create_by! key_field => key
 
-        setting.update(value_field => setting.instance_eval(&block)) if block
+        value = setting.instance_eval &(writer || default || -> {})
+
+        setting.update(value_field => value)
 
         setting.send value_field
       end

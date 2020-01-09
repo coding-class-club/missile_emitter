f46c72b4a89b0886da36fd65cc071ad5478d7eaf
Jerry Chen <chen.ruijie@gmail.com>
2020-01-09 09:49:33 +0800

    add option_names method

--- lib/missile_emitter/configurable.rb
+++ lib/missile_emitter/configurable.rb
@@ -3,10 +3,10 @@ module MissileEmitter
 
     # 配置项列表（klass => [key1, key2, ...]）
     # eg. {Setting => [:logo, :copyright, ...]}
-    option_names = {}
+    mapping = {}
     
     MissileEmitter do |klass, key_field = :key, value_field = :value, key, &default|
-      (option_names[klass] ||= [].to_set) << key
+      (mapping[klass] ||= [].to_set) << key
 
       klass.define_singleton_method key do |&writer|
         setting = find_or_initialize_by key_field => key
@@ -30,9 +30,14 @@ module MissileEmitter
     extend ActiveSupport::Concern
 
     included do
+      # 获取所有配置项名称：Klass.option_names ---> [:logo, copyright, ...]
+      define_singleton_method :option_names do
+        mapping.fetch self, []
+      end
+
       # 获取所有配置：Klass.options ---> {logo: '', copyright: '', ...}
       define_singleton_method :options do
-        option_names.fetch(self, []).each_with_object({}) do |key, result|
+        option_names.each_with_object({}) do |key, result|
           result[key] = send key
         end
       end
